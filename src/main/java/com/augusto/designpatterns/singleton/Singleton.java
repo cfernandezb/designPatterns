1ab970d62bae7c0e0bf309744c28ef337bfb3d3c
Ing. Cesar Fernandez <cfernandez@cellit.cl>
2025-01-11 22:17:47 -0400

    Feat: se crea clase basica singleton

---
+++ src/main/java/com/augusto/designpatterns/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package com.augusto.designpatterns.singleton;
+
+public class Singleton {
+    private static Singleton INSTANCE;
+
+    private Singleton() {}
+
+    public static Singleton getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
+    }
+}
+
